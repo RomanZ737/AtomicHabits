69d52fe35cbb364913a635d867b59cb77bb5bbf1
Roman <pomanz@mail.ru>
2026-08-23 12:58:00 +0300

    Done

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework import viewsets
+from .models import CustomUser
+from .permissions import IsOwner
+from .serializers import CustomUserSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
+
+    def get_permissions(self):
+        if self.action in ('update', 'partial_update', 'destroy'):
+            return [IsOwner()]
+        return super().get_permissions()
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
+    permission_classes = (AllowAny,)
